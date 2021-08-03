df4e01ead4c0bff3e653fd73c53356c712bd3018
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-08-03 19:17:10 +0400

    ayb

--- neon_installer/language.py
+++ neon_installer/language.py
@@ -5,7 +5,7 @@ from . import logo, console, bilgi
 def importlang ():
     console.clear()
     logo()
-    bilgi("\n\[1] Azərbaycanca\n\[2] Türkçe\n\[3] English\n\[4] Pусский")
+    bilgi("\n[1] Azərbaycanca\n[2] Türkçe\n[3] English\n[4] Pусский")
     Dil = Prompt.ask("[bold yellow]Xaiş bir dil seçin / Please select a language[/]", choices=["1", "2", "3", "4"], default="1")
 
     if Dil == "1":
@@ -28,4 +28,4 @@ def importlang ():
     return COUNTRY, LANGUAGE, TZ
 
 COUNTRY, LANGUAGE, TZ = importlang()
-LANG = loads(open(f"./alpha_installer/language/{LANGUAGE}.alphajson", "r").read())["STRINGS"]
+LANG = loads(open(f"./neon_installer/language/{LANGUAGE}.neonjson", "r").read())["STRINGS"]
